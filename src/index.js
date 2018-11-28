45af9bc12462e46d831b415912fb5c62a5686718
Russell Nyorere <ecodenhub@gmail.com>
2018-11-28 15:26:27 +0100

    App component Attribute removed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App';
-import data from './testData';
 
-ReactDom.render(<App contests={data.contests} />, document.getElementById('root'));
+ReactDom.render(<App />, document.getElementById('root'));
