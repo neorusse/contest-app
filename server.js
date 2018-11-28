fd8c68e2051d0f9d8bda60f0e093146c49a129cd
Russell Nyorere <ecodenhub@gmail.com>
2018-11-28 08:29:56 +0100

    Static Asset, EJS View Engine, and API Route configured

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import config from './config';
+import apiRouter from './api';
 import express from 'express';
 import logger from 'morgan';
 import chalk from 'chalk';
@@ -7,6 +8,11 @@ import chalk from 'chalk';
 const server = new express();
 
 server.use(logger('tiny'));
+server.use(express.static('public'));
+server.use('/api', apiRouter);
+
+server.set('view engine', 'ejs');
+
 server.get('/', (req, res) => {
   res.send('Whow!, Hello Express and EJS');
 });
