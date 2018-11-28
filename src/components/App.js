bb9b828e7ad631f1d9984b3290e6378666d8ba4f
Russell Nyorere <ecodenhub@gmail.com>
2018-11-28 15:28:53 +0100

    state data being fetcted from API using Axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,37 @@
 import React from 'react';
+import axios from 'axios';
 import Header from './Header';
 import ContestPreview from './ContestPreview';
+
+
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      pageHeader: 'Naming Contests'
+      pageHeader: 'Naming Contests',
+      contests: []
     };
+  }
+  componentDidMount() {
+    // ajax request
+    axios.get('/api/contests')
+      .then(res => {
+        this.setState({
+          contests: res.data.contests
+        });
+      })
+      .catch(console.error)
+  }
+  componentWillUnmount() {
+
   }
   render() {
     return (
       <div className="App">
         <Header message={this.state.pageHeader} />
         <div>
-        {this.props.contests.map(contest =>
+        {this.state.contests.map(contest =>
             <ContestPreview
               key={contest.id}
               {...contest}
