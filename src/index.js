2745a7766b3ab8f9df629321862b953e22eb5fa6
Russell Nyorere <ecodenhub@gmail.com>
2018-11-28 14:05:31 +0100

    JSON data injected dynamically into App component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App';
+import data from './testData';
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render(<App contests={data.contests} />, document.getElementById('root'));
