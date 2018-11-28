0fed394d553325e2bf0e26bada35e6845a235bb8
Russell Nyorere <ecodenhub@gmail.com>
2018-11-28 14:09:13 +0100

    compose ContentPreview Component for rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import ContestPreview from './ContestPreview';
 
 class App extends React.Component {
   constructor() {
@@ -13,7 +14,12 @@ class App extends React.Component {
       <div className="App">
         <Header message={this.state.pageHeader} />
         <div>
-
+        {this.props.contests.map(contest =>
+            <ContestPreview
+              key={contest.id}
+              {...contest}
+            />
+          )}
         </div>
       </div>
     );
