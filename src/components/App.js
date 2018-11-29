8f3103996f8d2f2e332af2ead566d23995caf07e
Russell Nyorere <ecodenhub@gmail.com>
2018-11-29 16:01:14 +0100

    Code refactored, & ContestList Component imported

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Header from './Header';
-import ContestPreview from './ContestPreview';
-
+import ContestList from './ContestList';
 
 
 class App extends React.Component {
@@ -30,14 +29,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header message={this.state.pageHeader} />
-        <div>
-        {this.state.contests.map(contest =>
-            <ContestPreview
-              key={contest.id}
-              {...contest}
-            />
-          )}
-        </div>
+        <ContestList contests={this.state.contests} />
       </div>
     );
   }
